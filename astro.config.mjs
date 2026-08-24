dc017d4a387c64cd6c7a61f92a9721a00ca425b1
Tim Tilch <tim.tilch@newcubator.com>
2026-08-24 21:52:57 +0200

    Build Astro assets for GitHub Pages path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.ankern-groemitz.de',
+  base: process.env.ASTRO_BASE || undefined,
   integrations: [sitemap()],
   trailingSlash: 'never'
 });
